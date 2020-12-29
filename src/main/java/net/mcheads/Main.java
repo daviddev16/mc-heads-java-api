234ab82d5f67d04d742d8efe93b4a7160f79b322
daviddev16 <davidduartepinheiro@gmail.com>
2020-12-29 16:19:43 -0300

    remove .class files.

--- src/main/java/net/mcheads/Main.java
+++ src/main/java/net/mcheads/Main.java
@@ -4,11 +4,12 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 import net.mcheads.api.IEntity;
 import net.mcheads.api.MCHeadsAPI;
-import net.mcheads.api.MHF;
 import net.mcheads.api.exceptions.EntityOutOfBoundsException;
+import net.mcheads.api.style.options.StyleOption;
 import net.mcheads.internal.Options;
 
 public class Main {
@@ -16,20 +17,20 @@ public class Main {
 	public static void main(String[] args) throws IOException, EntityOutOfBoundsException {
 
 		
-		IEntity player = MCHeadsAPI.getEntity(MHF.MHF_ARROWLEFT, Options.DEFAULT);
+		IEntity herobrine = MCHeadsAPI.getEntity("Daviddev", Options.FULL);
 
-	    System.out.println(player.getName());
-	    System.out.println(player.getUserId());
-	   // System.out.println(Arrays.toString(player.getNameHistory().getAllNames()));
+	   // System.out.println(herobrine.getName());
+	    //System.out.println(herobrine.getUserId());
+	    //System.out.println(Arrays.toString(herobrine.getNameHistory().getAllNames()));
 
-		byte[] face = player.getDefaultFacing();
+		byte[] isometricHead = herobrine.getIsometric(StyleOption.HEAD, 64); /*the raw byte array of the image*/
 
 		File imgFile = new File("img.png");
 		if(!imgFile.exists()) {
 			imgFile.createNewFile();
 		}
 
-		ByteArrayInputStream bis = new ByteArrayInputStream(face);
+		ByteArrayInputStream bis = new ByteArrayInputStream(isometricHead);
 		BufferedImage bImage2 = ImageIO.read(bis);
 		ImageIO.write(bImage2, "png", imgFile);
 
